e7415605edaa9f789b84e566bfb100f0a9daceeb
Dmitrii <alievd08@mail.ru>
2023-03-01 23:33:51 +0300

    Add command: Update

---
+++ src/main/java/org/example/Commands/Update.java
@@ -0,0 +1,18 @@
+package org.example.Commands;
+
+import java.util.ArrayList;
+
+public class Update extends Command{
+    private Long id;
+    @Override
+    public String execute(ArrayList<String> args) {
+        try{
+            this.id = Long.valueOf(args.get(0));
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("You need enter argument: id, {element} ");
+        } catch (NumberFormatException e){
+            System.out.println("Please enter number");
+        }
+        return null;
+    }
+}
