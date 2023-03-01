e63db38a9890df796907218422857d9d22f6504e
Dmitrii <alievd08@mail.ru>
2023-03-01 13:34:25 +0300

    Add method to start writing in console

--- src/main/java/org/example/ControlCenter.java
+++ src/main/java/org/example/ControlCenter.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import org.example.Commands.Command;
-import org.example.Commands.Help;
 
 import java.io.File;
 import java.util.HashMap;
@@ -16,15 +15,25 @@ public class ControlCenter {
 
         File dir = new File("src\\main\\java\\org\\example\\Commands");
         File[] arrFiles =dir.listFiles();
-
         for(File file:arrFiles){
             String ch = "\\\\";
             String[] arrCom = file.toString().split(ch);
             String name = arrCom[arrCom.length - 1].split("\\.")[0];
-            Class clazz = Class.forName("org.example.Commands." + name);
-            commandMap.put(name,(Command) clazz.newInstance());
-            System.out.println(commandMap.get(name).execute());
+            commandMap.put(name,(Command) Class.forName("org.example.Commands." + name).newInstance());
+
 
         }
+    }
+    public void Start (){
+        String command = scanner.nextLine();
+        while(!command.equalsIgnoreCase("Exit")){
+            
+            System.out.println(commandMap.get(firstUpperCase(command)).execute());
+            command = scanner.nextLine();
+        }
+    }
+    private String firstUpperCase(String word){
+        if(word == null || word.isEmpty()) return "";
+        return word.substring(0, 1).toUpperCase() + word.toLowerCase().substring(1);
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
         ControlCenter controlCenter = new ControlCenter();
+        controlCenter.Start();
     }
 }
