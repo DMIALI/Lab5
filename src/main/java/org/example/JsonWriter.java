2f69bae810f339a3d72ee1ecdc3c986a5151697d
Vasilii <vasilev_vasily_123@mail.ru>
2023-03-07 01:37:55 +0300

    reader and writer for file created

---
+++ src/main/java/org/example/JsonWriter.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import org.example.DataTypes.MusicBand;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonWriter {
+    public void write(File json, LinkedList<MusicBand> musicBands) throws IOException {
+        FileWriter fileWriter = new FileWriter(json);
+        String newjson = (new ObjectMapper()).writeValueAsString(musicBands);
+        System.out.println(newjson);
+        fileWriter.write(newjson);
+        fileWriter.flush();
+        fileWriter.close();
+    }
+}
+
