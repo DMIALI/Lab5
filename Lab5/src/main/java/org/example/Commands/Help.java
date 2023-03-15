0f070ae789b683ad22073997db1f9ea33c5d658f
6aculuu <115791178+6aculuu@users.noreply.github.com>
2023-03-15 23:01:40 +0300

    Update Help.java

--- Lab5/src/main/java/org/example/Commands/Help.java
+++ Lab5/src/main/java/org/example/Commands/Help.java
@@ -6,18 +6,14 @@ import org.example.utils.Printer;
 import java.util.*;
 
 public class Help extends Command {
-    private HashMap<String, Command> commandMap;
     public Help () {
         super("вывести справку по доступным командам");
-    }
-
-    public void addCommandMap(HashMap<String, Command> CommandMap){
-        this.commandMap = CommandMap;
     }
 
     @Override
     public void execute(InputCommandData input){
         Printer printer = input.printer();
+        HashMap<String, Command> commandMap = input.commandMap();
         StringBuilder out = new StringBuilder();
         Set <String> keys = commandMap.keySet();
         for (String key:keys){
