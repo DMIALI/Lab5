cf30988fb0db773aab121dc8fc07c33546978b44
6aculuu <115791178+6aculuu@users.noreply.github.com>
2023-03-15 23:29:27 +0300

    Update InputCommandData.java

--- Lab5/src/main/java/org/example/Commands/CommandData/InputCommandData.java
+++ Lab5/src/main/java/org/example/Commands/CommandData/InputCommandData.java
@@ -1,11 +1,12 @@
 package org.example.Commands.CommandData;
 
 import org.example.CollectionManager;
+import org.example.Commands.Command;
 import org.example.utils.Printer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
-public record InputCommandData(CollectionManager collectionManager, Scanner scanner, Printer printer, ArrayList<String> args) {
-
+public record InputCommandData(CollectionManager collectionManager, Scanner scanner, Printer printer, ArrayList<String> args, HashMap<String, Command> commandMap) {
 }
