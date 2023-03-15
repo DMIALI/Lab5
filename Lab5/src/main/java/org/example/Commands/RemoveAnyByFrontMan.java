c86c349462f0bfc018d137a71cfa6bf42068ec02
DMIALI <102038704+DMIALI@users.noreply.github.com>
2023-03-15 23:25:21 +0300

    Update RemoveAnyByFrontMan.java

--- Lab5/src/main/java/org/example/Commands/RemoveAnyByFrontMan.java
+++ Lab5/src/main/java/org/example/Commands/RemoveAnyByFrontMan.java
@@ -13,5 +13,20 @@ public class RemoveAnyByFrontMan extends Command {
 
     @Override
     public void execute(InputCommandData input) {
+        CollectionManager collectionManager = input.collectionManager();
+        Printer printer = input.printer();
+        ArrayList<String> args = input.args();
+        if(args.size() > 0) {
+            for(MusicBand musicBand: collectionManager.getMusicBands()){
+                if(musicBand.getFrontMan().getName().equals(args.get(0))){
+                    collectionManager.getMusicBands().remove(musicBand);
+                    return;
+                }
+            }
+            printer.errPrintln("frontMan с таким именем не найден =(");
+        }else{
+            printer.errPrintln("Введены некоректные данные");
+        }
+    }
     }
 }
